3afb15a4fd475da074e3966c45d38c4a85888d02
mfierlaf <43782114+mfierlaf@users.noreply.github.com>
2019-05-30 15:23:30 +0200

    Add files via upload

--- verif.c
+++ verif.c
@@ -6,36 +6,22 @@
 /*   By: tde-brit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 14:15:05 by tde-brit          #+#    #+#             */
-/*   Updated: 2019/05/28 15:07:52 by tde-brit         ###   ########.fr       */
+/*   Updated: 2019/05/30 15:15:18 by mfierlaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int	verif(char *line)
+int	occurence(char *line, int n, int j)
 {
-	int cpt;
-	int i;
-	int k;
-	int j;
-	int n;
-
-	cpt = 0;
-	k = 0;
-	j = 0;
-	n = 0;
-	if (line[20] != '\0' && line[20] != '\n')
-		return (0);
 	while (n < 20)
 	{
-		i = 0;
 		while (line[n] != '\0' && line[n] != '\n')
 		{
 			if (line[n] != '.' && line[n] != '#')
 				return (0);
 			if (line[n] == '#')
 			{
-				cpt++;
 				if (n != 21 && line[n] == line[n + 1])
 					j++;
 				if (n != 0 && line[n] == line[n - 1])
@@ -46,13 +32,70 @@ int	verif(char *line)
 					j++;
 			}
 			n++;
+		}
+		n++;
+	}
+	return (j);
+}
+
+int	ligne(char *line)
+{
+	int i;
+	int k;
+	int n;
+
+	k = 0;
+	n = 0;
+	while (n < 20)
+	{
+		i = 0;
+		while (line[n] != '\0' && line[n] != '\n')
+		{
+			n++;
 			i++;
 		}
 		n++;
 		k++;
 	}
-	if (cpt != 4 || i != 4 || k != 4)
+	if (i != 4 || k != 4)
 		return (0);
+	return (1);
+}
+
+int	compteur(char *line, int n)
+{
+	int cpt;
+
+	cpt = 0;
+	while (n < 20)
+	{
+		while (line[n] != '\0' && line[n] != '\n')
+		{
+			if (line[n] == '#')
+				cpt++;
+			n++;
+		}
+		n++;
+	}
+	if (cpt != 4)
+		return (0);
+	return (1);
+}
+
+int	verif(char *line)
+{
+	int j;
+	int n;
+
+	j = 0;
+	n = 0;
+	if (line[20] != '\0' && line[20] != '\n')
+		return (0);
+	if (compteur(line, n) != 1)
+		return (0);
+	if (ligne(line) != 1)
+		return (0);
+	j = occurence(line, n, j);
 	if (j == 6 || j == 8)
 		return (1);
 	else
